518e36a6cb4aa1fc09a1c2199718da1d6fd79f96
Damien Caliste <caliste@myrte>
2007-07-04 11:57:31 +0200

    Summary: add the use_time_reversal_at_gamma attribute
    Changes:
    	* config/etsf/specs.cf: upgrade the specifications to 2.2 by
    	  adding the use_time_reversal_at_gamma attribute.
    	* src/utils/etsf_io_tools.f90: implement two routines get and set
    	  to handle this new attribute with automatic tests.

--- src/utils/etsf_io_tools.f90
+++ src/utils/etsf_io_tools.f90
@@ -25,6 +25,9 @@ module etsf_io_tools
   private
 
   public :: etsf_io_tools_get_atom_names
+  
+  public :: etsf_io_tools_set_time_reversal_symmetry
+  public :: etsf_io_tools_get_time_reversal_symmetry
 
 contains
 
@@ -52,6 +55,7 @@ contains
 !! INPUTS
 !! * ncid = 
 !!     an opened NetCDF file with read access.
+!!
 !! OUTPUT
 !! * atom_names = an allocated array to store the atom names (indexed by species).
 !! * lstat = 
@@ -84,7 +88,10 @@ contains
     ! Read the array dimension.
     call etsf_io_low_read_dim(ncid, "number_of_atom_species", &
          & number_of_atom_species, lstat, error_data = error_data)
-    if (.not. lstat) return
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
 
     ! Check that argument size matches number_of_atoms.
     if (size(atom_names) /= number_of_atom_species) then
@@ -101,6 +108,7 @@ contains
     if (.not. lstat .and. error_data%access_mode_id /= ERROR_MODE_INQ) then
        ! Case where variable is found but has problems.
        deallocate(my_atom_numbers)
+       call etsf_io_low_error_update(error_data, me)
        return
     end if
     if (lstat) then
@@ -124,6 +132,7 @@ contains
          & atom_names, etsf_charlen, lstat, error_data = error_data)
     if (.not. lstat .and. error_data%access_mode_id /= ERROR_MODE_INQ) then
        ! Case where variable is found but has problems.
+       call etsf_io_low_error_update(error_data, me)
        return
     end if
     if (lstat) then
@@ -144,6 +153,7 @@ contains
     if (.not. lstat .and. error_data%access_mode_id /= ERROR_MODE_INQ) then
        ! Case where variable is found but has problems.
        deallocate(symbols)
+       call etsf_io_low_error_update(error_data, me)
        return
     end if
     if (lstat) then
@@ -164,6 +174,191 @@ contains
             & errmess = "no variable exists, can't get atom names.")
     end if
   end subroutine etsf_io_tools_get_atom_names
+!!***
+
+!!****m* etsf_io_tools/etsf_io_tools_get_time_reversal_symmetry
+!! NAME
+!!  etsf_io_tools_get_time_reversal_symmetry
+!!
+!! FUNCTION
+!!  In the specifications, an attribute can be set to describe if
+!!  the basis set informations have been reduced using the time reversal symmetry
+!!  at Gamma point in the case of plane wave basis sets. This routine poll the given
+!!  file, check that the basis set is plane waves and check that the attribute is set
+!!  coherently between variables.
+!!
+!! COPYRIGHT
+!!  Copyright (C) 2006, 2007 (Damien Caliste)
+!!  This file is distributed under the terms of the
+!!  GNU Lesser General Public License, see the COPYING file
+!!  or http://www.gnu.org/copyleft/lesser.txt .
+!!
+!! INPUTS
+!! * ncid = an opened NetCDF file with read access.
+!!
+!! OUTPUT
+!! * symmetry = .false. if the symmetry is not used.
+!! * lstat = 
+!!     return .true. if all the actions succeed (especially checks).
+!! * error_data <type(etsf_io_low_error)> = 
+!!     contains the details of the error is @lstat is false.
+!!
+!! SOURCE
+  subroutine etsf_io_tools_get_time_reversal_symmetry(ncid, symmetry, &
+       & lstat, error_data)
+    integer, intent(in)                        :: ncid
+    logical, intent(out)                       :: lstat, symmetry
+    type(etsf_io_low_error), intent(out)       :: error_data
+
+    ! Local variables
+    character(len = *), parameter :: me = "etsf_io_tools_get_time_reversal_symmetry"
+    character(len = etsf_charlen) :: basis, att1, att2
+
+    ! Read the basis set definition.
+    call etsf_io_low_read_var(ncid, "basis_set", &
+         & basis, etsf_charlen, lstat, error_data = error_data)
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
+    call strip(basis)
+
+    ! Check that basis set is indeed plane waves.
+    if (trim(basis) /= "plane_waves") then
+       call etsf_io_low_error_set(error_data, ERROR_MODE_SPEC, ERROR_TYPE_ATT, me, &
+            & tgtname = "basis_set", &
+            & errmess = "time reversal is only associated to plane wave basis sets.")
+       lstat = .false.
+       return
+    end if
+
+    ! Read the two attributes for reduced_coordinates_of_plane_waves and
+    ! coefficients_of_wavefunctions.
+    call etsf_io_low_read_att(ncid, "reduced_coordinates_of_plane_waves", &
+         & "use_time_reversal_at_gamma", etsf_charlen, att1, lstat, &
+         & error_data = error_data)
+    if (.not. lstat .and. error_data%access_mode_id /= ERROR_MODE_INQ .and. &
+         & error_data%target_type_id /= ERROR_TYPE_ATT) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    else
+       if (.not.lstat) write(att1, "(A)") "no"
+    end if
+    call etsf_io_low_read_att(ncid, "coefficients_of_wavefunctions", &
+         & "use_time_reversal_at_gamma", etsf_charlen, att2, lstat, &
+         & error_data = error_data)
+    if (.not. lstat .and. error_data%access_mode_id /= ERROR_MODE_INQ .and. &
+         & error_data%target_type_id /= ERROR_TYPE_ATT) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    else
+       if (.not.lstat) write(att2, "(A)") "no"
+    end if
+
+    ! Check the consistency of the values.
+    if (.not.(att1(1:1) == "n" .and. att2(1:1) == "n") .and. &
+         & .not.(att1(1:2) == "y" .and. att2(1:1) == "y")) then
+       call etsf_io_low_error_set(error_data, ERROR_MODE_SPEC, ERROR_TYPE_ATT, me, &
+            & tgtname = "use_time_reversal_at_gamma", &
+            & errmess = "attributes use_time_reversal_at_gamma have wrong values.")
+       lstat = .false.
+       return
+    end if
+
+    ! Everything is OK, we return.
+    symmetry = (att1(1:1) == "y")
+    lstat =.true.
+  end subroutine etsf_io_tools_get_time_reversal_symmetry
+!!***
+
+!!****m* etsf_io_tools/etsf_io_tools_set_time_reversal_symmetry
+!! NAME
+!!  etsf_io_tools_set_time_reversal_symmetry
+!!
+!! FUNCTION
+!!  In the specifications, an attribute can be set to describe if
+!!  the basis set informations have been reduced using the time reversal symmetry
+!!  at Gamma point in the case of plane wave basis sets. This routine set the given
+!!  value and check that the basis set is plane waves.
+!!
+!!  The file will be put in define mode.
+!!
+!! COPYRIGHT
+!!  Copyright (C) 2006, 2007 (Damien Caliste)
+!!  This file is distributed under the terms of the
+!!  GNU Lesser General Public License, see the COPYING file
+!!  or http://www.gnu.org/copyleft/lesser.txt .
+!!
+!! INPUTS
+!! * ncid = an opened NetCDF file with write access.
+!! * symmetry = the symmetry status.
+!!
+!! OUTPUT
+!! * lstat = 
+!!     return .true. if all the actions succeed (especially checks).
+!! * error_data <type(etsf_io_low_error)> = 
+!!     contains the details of the error is @lstat is false.
+!!
+!! SOURCE
+  subroutine etsf_io_tools_set_time_reversal_symmetry(ncid, symmetry, &
+       & lstat, error_data)
+    integer, intent(in)                        :: ncid
+    logical, intent(in)                        :: symmetry
+    logical, intent(out)                       :: lstat
+    type(etsf_io_low_error), intent(out)       :: error_data
+
+    ! Local variables
+    character(len = *), parameter :: me = "etsf_io_tools_set_time_reversal_symmetry"
+    character(len = etsf_charlen) :: basis, att
+
+    ! Read the basis set definition.
+    call etsf_io_low_read_var(ncid, "basis_set", &
+         & basis, etsf_charlen, lstat, error_data = error_data)
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
+    call strip(basis)
+
+    ! Check that basis set is indeed plane waves.
+    if (trim(basis) /= "plane_waves") then
+       call etsf_io_low_error_set(error_data, ERROR_MODE_SPEC, ERROR_TYPE_ATT, me, &
+            & tgtname = "basis_set", &
+            & errmess = "time reversal is only associated to plane wave basis sets.")
+       lstat = .false.
+       return
+    end if
+
+    ! We switch to define mode.
+    call etsf_io_low_set_define_mode(ncid, lstat, error_data)
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
+
+    ! Write the two attributes for reduced_coordinates_of_plane_waves and
+    ! coefficients_of_wavefunctions.
+    if (symmetry) then
+       write(att, "(A)") "yes"
+    else
+       write(att, "(A)") "no"
+    end if
+    call etsf_io_low_write_att(ncid, "reduced_coordinates_of_plane_waves", &
+         & "use_time_reversal_at_gamma", att, lstat, error_data)
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
+    call etsf_io_low_write_att(ncid, "coefficients_of_wavefunctions", &
+         & "use_time_reversal_at_gamma", att, lstat, error_data)
+    if (.not. lstat) then
+       call etsf_io_low_error_update(error_data, me)
+       return
+    end if
+
+    ! Everything is OK, we return.
+    lstat =.true.
+  end subroutine etsf_io_tools_set_time_reversal_symmetry
 !!***
 
 end module etsf_io_tools
