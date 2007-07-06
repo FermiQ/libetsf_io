4ce9527a37182598b1e9dbd02fd1a8e69ec5055f
Damien Caliste <caliste@myrte>
2007-07-06 13:56:03 +0200

    Summary: rewrite the tests for groups to be able to test the copy action
    Changes:
    	* config/etsf/template.tests_read,
    	  config/etsf/template.tests_write &
    	  config/etsf/template.tests: merge the two first into the third.
    	* tests/group_level/tests_copy.f90: this file is now auto-
    	  generated.
    	* ChangeLog & TODO: update these two files.
    	* config/etsf/library.cf: add some missing documentation for
    	  IO variables in the etsf_io_data routines.
    	* config/etsf/template.low_level_var: add a missing error
    	  propagator:
    	* config/scripts/autogen_subroutines.py: add the retrieval of
    	  the attribute names in the etsf_io_data_contents routine.
    	* config/scripts/autogen_tests.py: rewrite of the tests. Add
    	  also several tests for the copy routines.
    	* tests/group_level/Makefile.am: update the Makefile.
    	* tests/group_level/test_split_electrons_part*: change some
    	  values to be able to treat it automatically later.

--- config/scripts/autogen_tests.py
+++ config/scripts/autogen_tests.py
@@ -92,35 +92,29 @@ def init_grp(grp_name, grp_id):
   ret += "call tests_write_status(\" | closing\", lstat, error)\n\n"
   return indent_code(ret, 2)
 
-def create_variables(grp_name, grp_id):
-  # create the declaration of variables used by this grp
-  ret = "type(etsf_%s), target :: group\n" % grp_name
-  for var in etsf_groups[grp_name]:
-    var_dsc = etsf_variables[var]
-    # Retrieve variable properties of interest.
-    unformatted = False
-    splitted = False
-    if (var in etsf_properties):
-      props = etsf_properties[var]
-      unformatted = ( props & ETSF_PROP_VAR_UNFORMATTED == ETSF_PROP_VAR_UNFORMATTED)
-      splitted    = ( props & ETSF_PROP_VAR_SUB_ACCESS == ETSF_PROP_VAR_SUB_ACCESS)
-    # All variables for the test will be allocatable arrays.
-    ret += "%s, allocatable, target :: %s(" % (fortran_type(var_dsc), var)
-    
-    # If the variable is splitted or unformatted, we
-    # use 1D array as target.
-    if (not(unformatted) and not(splitted)):
-      if (var_dsc[0].startswith("string")):
-        stop = -2
-      else:
-        stop = -1
-      for i in var_dsc[1:stop]:
-        ret += ":,"
-    ret += ":)\n"
-  ret += "\n"
-  return ret
+def output_wrong_statement(action):
+  if (action == "copy"):
+    ret  = "call etsf_io_data_copy(\"Makefile\", \"pouet\", dims, lstat, error)\n"
+    ret += "call tests_status(\"source_file: wrong value (no file)\", (.not. lstat), error)\n"
+    ret += "\n"
+    ret += "call etsf_io_data_copy(\"Makefile\", \"Makefile\", dims, lstat, error)\n"
+    ret += "call tests_status(\"source_file: wrong value (text file)\", (.not. lstat), error)\n"
+    ret += "\n"
+    ret += "call etsf_io_data_copy(\"pouet\", \"test_write_geometry.nc\", dims, lstat, error)\n"
+    ret += "call tests_status(\"dest_file: wrong value (no file)\", (.not. lstat), error)\n"
+    ret += "\n"
+    ret += "call etsf_io_data_copy(\"Makefile\", \"test_write_geometry.nc\", dims, lstat, error)\n"
+    ret += "call tests_status(\"dest_file: wrong value (text file)\", (.not. lstat), error)\n"
+    return ret
+  else:
+    ret  = "call etsf_io_data_%s(\"pouet\", grp, lstat, error)\n" % action
+    ret += "call tests_status(\"dest_file: wrong value (no file)\", (.not. lstat), error)\n"
+    ret += "\n"
+    ret += "call etsf_io_data_%s(\"Makefile\", grp, lstat, error)\n" % action
+    ret += "call tests_status(\"dest_file: wrong value (text file)\", (.not. lstat), error)\n"
+    return ret
   
-def output_allocate_init_statement(var):
+def output_init_statement(var):
   # Retrieve variable properties of interest.
   unformatted = False
   splitted = False
@@ -131,167 +125,86 @@ def output_allocate_init_statement(var):
 
   ret = ""
   var_dsc = etsf_variables[var]
-  # Case the variable is a scalar
-  if ((var_dsc[0].startswith("string") and len(var_dsc) < 3) or \
-  (not(var_dsc[0].startswith("string")) and len(var_dsc) < 2)):
-    ret += "allocate(%s(1))\n" % var
-    if (var_dsc[0].startswith("string")):
-      ret += "write(%s(1), \"(A)\") \"He\"\n" % var
-    else:
-      ret += "%s(1) = 456\n" % var
-    return ret
-  
-  
-  # The variable is a dimension
-  if (var_dsc[0].startswith("string")):
-    stop = -2
-  else:
-    stop = -1
+
   # We compute the total length of data
-  ret += "length = &\n"
-  for dim in var_dsc[1:stop]:
-    ret += "  & dims%%%s * &\n" % dim
-  ret += "  & dims%%%s\n" % var_dsc[stop]
   if (splitted or unformatted):
-    # We allocate.
-    ret += "allocate(%s(length))\n" % var
-    if (var_dsc[0].startswith("string")):
-      ret += "write(%s, \"(A)\") \"He\"\n" % var
-    else:
-      ret += "%s = (/ (i, i = 1, length) /)\n" % var
+    ret += "call tests_init_variable(group%%%s%%data1D, (/ &\n" % var
+    for dim in var_dsc[1:-1]:
+      ret += "  & dims%%%s * &\n" % dim
+    ret += "  & dims%%%s /))\n" % var_dsc[-1]
   else:
     # We allocate with the fixed shape in a reverse order.
     var_dsc_cpy = var_dsc[1:]
     var_dsc_cpy.reverse()
-    ret += "allocate(%s( &\n" % var
-    for dim in var_dsc_cpy[-1 - stop:-1]:
-      ret += "  & dims%%%s, &\n" % dim
-    ret += "  & dims%%%s))\n" % var_dsc_cpy[-1]
-    if (var_dsc[0].startswith("string")):
-      # Only 1D array of strings are supported.
-      ret += "do i = 1, dims%%%s, 1\n" % var_dsc[1]
-      ret += "  write(%s(i), \"(A)\") \"He\"\n" % var
-      ret += "end do\n"
+    if (len(var_dsc) == 1):
+      ret += "call tests_init_variable(group%%%s)\n" % var
     else:
-      # We put values with a reshape.
-      ret += "%s = reshape( (/ (i, i = 1, length) /) , (/ & \n" % var
-      for dim in var_dsc_cpy[-1 - stop:-1]:
+      ret += "call tests_init_variable(group%%%s, (/ &\n" % var
+      for dim in var_dsc_cpy[0:-1]:
         ret += "  & dims%%%s, &\n" % dim
-      ret += "  & dims%%%s /) )\n" % var_dsc_cpy[-1]
+      ret += "  & dims%%%s /))\n" % var_dsc[1]
   return ret
-  
-def output_associate_statement(var):
-  # Retrieve variable properties of interest.
-  unformatted = False
-  splitted = False
-  if (var in etsf_properties):
-    props = etsf_properties[var]
-    unformatted = ( props & ETSF_PROP_VAR_UNFORMATTED == ETSF_PROP_VAR_UNFORMATTED)
-    splitted    = ( props & ETSF_PROP_VAR_SUB_ACCESS == ETSF_PROP_VAR_SUB_ACCESS)
 
-  ret = ""
-  if (unformatted or splitted):
-    ret += "group%%%s%%data1D => %s\n" % (var, var)
-  else:
-    var_dsc = etsf_variables[var]
-    if ((var_dsc[0].startswith("string") and len(var_dsc) < 3) or \
-    (not(var_dsc[0].startswith("string")) and len(var_dsc) < 2)):
-      ret += "group%%%s => %s(1)\n" % (var, var)
-    else:
-      ret += "group%%%s => %s\n" % (var, var)
-  return ret
-  
 def output_check_statement(var, action):
-  # Retrieve variable properties of interest.
-  unformatted = False
-  splitted = False
-  if (var in etsf_properties):
-    props = etsf_properties[var]
-    unformatted = ( props & ETSF_PROP_VAR_UNFORMATTED == ETSF_PROP_VAR_UNFORMATTED)
-    splitted    = ( props & ETSF_PROP_VAR_SUB_ACCESS == ETSF_PROP_VAR_SUB_ACCESS)
-
   ret = ""
   var_dsc = etsf_variables[var]
-  if (var_dsc[0].startswith("string")):
-    char_len = "dims%%%s, " % var_dsc[-1]
-    stop = -2
-  else:
-    char_len = ""
-    stop = -1
-  
-  # Case the variable is a scalar
-  if ((var_dsc[0].startswith("string") and len(var_dsc) < 3) or \
-  (not(var_dsc[0].startswith("string")) and len(var_dsc) < 2)):
-    if (action == "write"):
-      # In write mode, we read the value.
-      ret += "call etsf_io_low_read_var(ncid, \"%s\", %s(1), &\n" % (var, var)
-      ret += "                        & %slstat, error_data = error_data)\n" % char_len
-      ret += "call tests_%s_status(\" | read '%s' values\", lstat, error_data)\n" % (action, var)
-    # We make the test.
+
+  if (action == "read"):
+    # Retrieve variable properties of interest.
     if (var_dsc[0].startswith("string")):
-      ret += "if (%s(1)(1:index(%s(1), char(0)) - 1) /= \"He\") then\n" % (var, var)
+      if (len(var_dsc) == 2):
+        ret += "call tests_check_values(group%%%s, dims%%%s, \"%s\", &\n" % (var, var_dsc[1], var)
+        ret += "                      & lstat, error_data)\n"
+      else:
+        ret += "call tests_check_values(group%%%s, (/ dims%%%s, dims%%%s /), &\n" % (var, var_dsc[2], var_dsc[1])
+        ret += "                      & \"%s\", lstat, error_data)\n" % var
     else:
-      ret += "if (int(%s(1)) /= 456) then\n" % var
-    ret += "  call etsf_io_low_error_set(error_data, ERROR_MODE_SPEC, ERROR_TYPE_VAR, me, &\n"
-    ret += "                           & tgtname = \"%s\", errmess = \"wrong values\")\n" %var
-    ret += "  lstat = .false.\n"
-    ret += "end if\n"
-    ret += "call tests_%s_status(\" | check '%s' values\", lstat, error_data)\n" % (action, var)
+      unformatted = False
+      splitted = False
+      if (var in etsf_properties):
+        props = etsf_properties[var]
+        unformatted = ( props & ETSF_PROP_VAR_UNFORMATTED == ETSF_PROP_VAR_UNFORMATTED)
+        splitted    = ( props & ETSF_PROP_VAR_SUB_ACCESS == ETSF_PROP_VAR_SUB_ACCESS)
+      if (unformatted or splitted):
+        ret += "call tests_check_values(group%%%s%%data1D, \"%s\", &\n" % (var, var)
+        ret += "                      & lstat, error_data)\n"
+      else:
+        if (len(var_dsc) > 2):
+          ret += "call tests_check_values(reshape(group%%%s, (/ 1 /)), \"%s\", &\n" % (var, var)
+          ret += "                      & lstat, error_data)\n"
+        else:
+          ret += "call tests_check_values(group%%%s, \"%s\", &\n" % (var, var)
+          ret += "                      & lstat, error_data)\n"
+    return ret
+  else:
+    # Case the variable is a scalar
+    if (len(var_dsc) < 2):
+      ret += "call tests_check_variable(ncid, \"%s\", \"%s\", lstat, error_data)\n" % (var, var_dsc[0])
+      return ret
+
+    # Case of a dimension
+    var_dsc_cpy = var_dsc[1:]
+    var_dsc_cpy.reverse()
+    ret += "call tests_check_variable(ncid, \"%s\", \"%s\", (/ &\n" % (var, var_dsc[0])
+    for dim in var_dsc_cpy[0:-1]:
+      ret += "  & dims%%%s, &\n" % dim
+    ret += "  & dims%%%s /), lstat, error_data)\n" % var_dsc_cpy[-1]
     return ret
   
-  # case of array
-  if (action == "write"):
-    # In write mode, we read the value.
-    ret += "call etsf_io_low_read_var(ncid, \"%s\", %s, &\n" % (var, var)
-    ret += "                        & %slstat, error_data = error_data)\n" % char_len
-    ret += "call tests_write_status(\" | read '%s' values\", lstat, error_data)\n" % var
-  # We make the test.
-  if (var_dsc[0].startswith("string")):
-    ret += "lstat = .true.\n"
-    ret += "do i = 1, dims%%%s, 1\n" % var_dsc[1]
-    ret += "  if (index(%s(i), char(0)) > 0) then\n" % var
-    ret += "    lstat = (%s(i)(1:index(%s(i), char(0)) - 1) == \"He\") .and. lstat\n" % (var, var)
-    ret += "  else\n"
-    ret += "    lstat = (trim(%s(i)) == \"He\") .and. lstat\n" % var
-    ret += "  end if\n"
-    ret += "end do\n"
-  else:
-    ret += "length = &\n"
-    for dim in var_dsc[1:stop]:
-      ret += "  & dims%%%s * &\n" % dim
-    ret += "  & dims%%%s\n" % var_dsc[stop]
-    if (var_dsc[0].startswith("integer")):
-      tab = "int"
-    else:
-      tab = "dbl"
-    ret += "allocate(test_%s_tab(length))\n" % tab
-    ret += "test_%s_tab = reshape(%s, (/ length /))\n" % (tab, var)
-    ret += "lstat = .true.\n"
-    ret += "do i = 1, length, 1\n"
-    ret += "  lstat = (int(test_%s_tab(i)) == i) .and. lstat\n" % tab
-    ret += "end do\n"
-    ret += "deallocate(test_%s_tab)\n" % tab
-  ret += "if (.not. lstat) then\n"
-  ret += "  call etsf_io_low_error_set(error_data, ERROR_MODE_SPEC, ERROR_TYPE_VAR, me, &\n"
-  ret += "                           & tgtname = \"%s\", errmess = \"wrong values\")\n" %var
-  ret += "  lstat = .false.\n"
-  ret += "end if\n"
-  ret += "call tests_%s_status(\" | check '%s' values\", lstat, error_data)\n" % (action, var)
-  return ret
-  
-def readwrite_grp(grp_name, grp_id, action):
-  """ Action argument is either "write" or "read"."""
+def action_grp(grp_name, grp_id, action):
+  """ Action argument can be "write", "read" or "copy"."""
   ret = "subroutine test_%s_%s()\n" % (action, grp)
   ret += "  type(etsf_dims) :: dims\n"
   ret += "  type(etsf_groups) :: groups\n"
   ret += "  type(etsf_groups_flags) :: flags\n"
-  ret += "  integer :: i, length, ncid\n"
+  ret += "  type(etsf_%s), target :: group\n" % grp
+  
   ret += "  logical :: lstat\n"
-  ret += "  integer, allocatable :: test_int_tab(:)\n"
-  ret += "  double precision, allocatable :: test_dbl_tab(:)\n"
-  ret += "  character(len = *), parameter :: me = \"test_%s_%s\"\n" % (action, grp_name)
   ret += "  type(etsf_io_low_error) :: error_data\n"
-  ret += indent_code(create_variables(grp_name, grp_id), 1)
+  
+  ret += "  character(len = *), parameter :: me = \"test_%s_%s\"\n" % (action, grp_name)
+  ret += "  integer :: ncid\n"
+  
   # We create a file for this group.
   ret += """
   groups%%%s => group
@@ -299,63 +212,91 @@ def readwrite_grp(grp_name, grp_id, action):
   write(*,*)
   write(*,*) "Testing etsf_io_data_%s()..."
 
-  dims%%number_of_grid_points_vector3 = 5
-  dims%%number_of_symmetry_operations = 4
-  dims%%max_number_of_coefficients = 6
-  dims%%max_number_of_projectors = 2
-  dims%%max_number_of_states = 8
-  dims%%number_of_atoms = 4
-  dims%%number_of_kpoints = 12
-  dims%%number_of_components = 2
 """ % (grp_name, action)
-  ret += "flags%%%s = etsf_%s_all\n" % (grp_name, grp_name)
-  ret += "call etsf_io_data_init(\"test_%s_%s.nc\", flags, &\n" % (action, grp_name)
-  ret += "                     & dims, \"Test\", \"\", lstat, error_data)\n"
-  ret += "call tests_%s_status(\"Create file test_%s_%s.nc\", lstat, error_data)\n" % (action, action, grp_name)
+
+  if (action == "copy"):
+    ret += "call etsf_io_low_open_read(ncid, \"test_write_%s.nc\", lstat, error_data = error_data)\n" % grp_name
+    ret += "call etsf_io_dims_get(ncid, dims, lstat, error_data)\n"
+    ret += "call etsf_io_low_close(ncid, lstat, error_data)\n"
+  else:
+    ret += "dims%number_of_grid_points_vector3 = 5\n"
+    ret += "dims%number_of_symmetry_operations = 4\n"
+    ret += "dims%max_number_of_coefficients = 6\n"
+    ret += "dims%max_number_of_projectors = 2\n"
+    ret += "dims%max_number_of_states = 8\n"
+    ret += "dims%number_of_atoms = 5\n"
+    ret += "dims%number_of_atom_species = 3\n"
+    ret += "dims%number_of_kpoints = 12\n"
+    ret += "dims%number_of_components = 2\n"
+  
+  ret += "\n"
+  
+  ret += "  flags%%%s = etsf_%s_all\n" % (grp_name, grp_name)
+  ret += "  call etsf_io_data_init(\"test_%s_%s.nc\", flags, &\n" % (action, grp_name)
+  ret += "                       & dims, \"Test\", \"\", lstat, error_data)\n"
+  ret += "  call tests_status(\"Create file test_%s_%s.nc\", lstat, error_data)\n" \
+         % (action, grp_name)
+  
   # We allocate space for the data to be written or read and
   # we put some values in it then we associate the data.
-  for var in etsf_groups[grp_name]:
-    # We allocate and put init values.
-    ret += "! Allocate and init %s\n" % var
-    ret += output_allocate_init_statement(var)
-    # We associate
-    ret += "! Associate %s\n" % var
-    # If the var is neither unformatted nor splitted, we associate directly
-    ret += output_associate_statement(var)
+  if (action != "copy"):
+    for var in etsf_groups[grp_name]:
+      # We allocate and put init values.
+      ret += "  ! Allocate and init %s\n" % var
+      ret += indent_code(output_init_statement(var), 1)
     
-  ret += "\n"
-  # we call the write routine (both for testing or to write for future read testing).
-  ret += "call etsf_io_data_write(\"test_%s_%s.nc\", &\n" % (action, grp_name)
-  ret += "                      & groups, lstat, error_data)\n"
-  ret += "call tests_%s_status(\"write data\", lstat, error_data)\n" % action
+    ret += "\n"
+    # we call the write routine (both for testing or to write
+    # for future read testing).
+    ret += "  call etsf_io_data_write(\"test_%s_%s.nc\", &\n" % (action, grp_name)
+    ret += "                        & groups, lstat, error_data)\n"
+    ret += "  call tests_status(\"write data\", lstat, error_data)\n"
+  else:
+    # we call the copy routine.
+    ret += "  call etsf_io_data_copy(\"test_%s_%s.nc\", \"test_write_%s.nc\", &\n" % \
+           (action, grp_name, grp_name)
+    ret += "                        & dims, lstat, error_data)\n"
+    ret += "  call tests_status(\"copy data to test_%s_%s.nc\", lstat, error_data)\n" % (action, grp_name)
   
   # We open the file for low level access
-  ret += "! check informations.\n"
-  if (action == "write"):
+  ret += "  ! check informations.\n"
+  if (action == "write" or action == "copy"):
     # We check the previous write action with low level reading
-    ret += "call etsf_io_low_open_read(ncid, \"test_write_%s.nc\", lstat, error_data = error_data)\n" % (grp_name)
-    ret += "call tests_write_status(\" | opening\", lstat, error_data)\n"
-  else:
+    ret += "  call etsf_io_low_open_read(ncid, \"test_%s_%s.nc\", lstat, error_data = error_data)\n" % (action, grp_name)
+    ret += "  call tests_status(\" | opening\", lstat, error_data)\n"
+  elif (action == "read"):
     # we call the read action for testing purpose
-    ret += "call etsf_io_data_read(\"test_read_%s.nc\", &\n" % grp_name
-    ret += "                     & groups, lstat, error_data)\n"
-    ret += "call tests_read_status(\"read data\", lstat, error_data)\n"
+    ret += "  call etsf_io_data_read(\"test_%s_%s.nc\", &\n" % (action, grp_name)
+    ret += "                       & groups, lstat, error_data)\n"
+    ret += "  call tests_status(\"read data\", lstat, error_data)\n"
   
   # We will check the values
   for var in etsf_groups[grp_name]:
     ret += output_check_statement(var, action)
   
-  if (action == "write"):
+  if (action == "write" or action == "copy"):
     # We close the file
     ret += """
   ! close file
   call etsf_io_low_close(ncid, lstat, error_data = error_data)
-  call tests_write_status(" | closing", lstat, error_data)
+  call tests_status(" | closing", lstat, error_data)
+  
 """
   # We deallocate
-  for var in etsf_groups[grp_name]:
-    ret += "deallocate(%s)\n" % (var)
-  ret += "  write(*,*)\n"
+  if (action != "copy"):
+    for var in etsf_groups[grp_name]:
+      # Retrieve variable properties of interest.
+      unformatted = False
+      splitted = False
+      if (var in etsf_properties):
+        props = etsf_properties[var]
+        unformatted = ( props & ETSF_PROP_VAR_UNFORMATTED == ETSF_PROP_VAR_UNFORMATTED)
+        splitted    = ( props & ETSF_PROP_VAR_SUB_ACCESS == ETSF_PROP_VAR_SUB_ACCESS)
+      if (unformatted or splitted):
+        ret += "  deallocate(group%%%s%%data1D)\n" % (var)
+      else:
+        ret += "  deallocate(group%%%s)\n" % (var)
+  ret += "\n  write(*,*)\n"
   ret += "end subroutine test_%s_%s\n\n" % (action, grp)
   return indent_code(ret, 1)
 
@@ -404,42 +345,25 @@ out = file("%s/tests_init.f90" % (etsf_tests_srcdir),"w")
 out.write(ret)
 out.close()
 
-# Create tests for data_write()
-# =============================
-write_grp_src = ""
-call_grp_src = ""
-for grp in etsf_group_list:
-  # Load template
-  write_grp_src += readwrite_grp(grp, "etsf_grp_" + grp, "write")
-  call_grp_src += "  call test_write_%s()\n" % grp
+for action in ["write", "read", "copy"]:
+  # Create tests for data_action()
+  # =============================
+  action_grp_src = ""
+  call_grp_src = ""
+  for grp in etsf_group_list:
+    # Load template
+    action_grp_src += action_grp(grp, "etsf_grp_" + grp, action)
+    call_grp_src += "  call test_%s_%s()\n" % (action, grp)
 
-ret = file("config/etsf/template.tests_write", "r").read()
+  ret = file("config/etsf/template.tests", "r").read()
 
-# Substitute patterns
-ret = re.sub("@WRITE_GRP@", write_grp_src, ret)
-ret = re.sub("@CALL_WRITE_GROUP@", call_grp_src, ret)
+  # Substitute patterns
+  ret = re.sub("@SUBROUTINE_GRP@", action_grp_src, ret)
+  ret = re.sub("@TEST_GRP@", output_wrong_statement(action), ret)
+  ret = re.sub("@CALL_GROUP@", call_grp_src, ret)
+  ret = re.sub("@ACTION@", action, ret)
 
-# Write routine
-out = file("%s/tests_write.f90" % (etsf_tests_srcdir),"w")
-out.write(ret)
-out.close()
-
-# Create tests for data_read()
-# =============================
-read_grp_src = ""
-call_grp_src = ""
-for grp in etsf_group_list:
-  # Load template
-  read_grp_src += readwrite_grp(grp, "etsf_grp_" + grp, "read")
-  call_grp_src += "  call test_read_%s()\n" % grp
-
-ret = file("config/etsf/template.tests_read", "r").read()
-
-# Substitute patterns
-ret = re.sub("@READ_GRP@", read_grp_src, ret)
-ret = re.sub("@CALL_READ_GROUP@", call_grp_src, ret)
-
-# Write routine
-out = file("%s/tests_read.f90" % (etsf_tests_srcdir),"w")
-out.write(ret)
-out.close()
+  # Write routine
+  out = file("%s/tests_%s.f90" % (etsf_tests_srcdir, action),"w")
+  out.write(ret)
+  out.close()
